e998ab6233e0f3143c664d04049a5ae59b77ef9e
asket21 <kronarkrouse@gmail.com>
2024-05-22 23:52:22 +0300

    link

--- app/api/config.js
+++ app/api/config.js
@@ -1,8 +1,8 @@
-export const BASE_URL = "https://pindieback-asket21.nomoredomainswork.ru/";
+export const BASE_URL = "https://pindieback-asket21.nomoredomainswork.ru";
 
 export const endpoints = {
-    games: `${BASE_URL}/games`,
-    auth: `${BASE_URL}/auth/login`,
-    me: `${BASE_URL}/me`,
-    reg:`${BASE_URL}/auth/local/register`
+  games: `${BASE_URL}/games`,
+  auth: `${BASE_URL}/auth/login`,
+  me: `${BASE_URL}/me`,
+  reg: `${BASE_URL}/auth/local/register`,
 };
